252492863e2b34186688e9abcd64f5dc5ebce944
Maxim Zviagintsev <maxim.zviagintsev@gmail.com>
2023-05-10 18:07:48 +0300

    Configure eslint ts prettier

---
+++ client/src/.eslint.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+};
